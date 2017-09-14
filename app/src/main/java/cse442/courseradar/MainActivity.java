811071fbe5b6bf8c186fd87911f6b40933d1e135
junjieChen0608 <wiiso853@gmail.com>
2017-09-14 19:41:32 -0400

    fixed: courseData HashMap key-value pair
    Firebase only supports String->Object Mapping

--- app/src/main/java/cse442/courseradar/MainActivity.java
+++ app/src/main/java/cse442/courseradar/MainActivity.java
@@ -2,12 +2,62 @@ package cse442.courseradar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private DatabaseReference firebaseDB;
+    private Button btnGetData;
+    private TextView tvCourseData;
+    private EditText etChildPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firebaseDB = FirebaseDatabase.getInstance().getReference();
+        btnGetData = (Button) findViewById(R.id.get_data);
+        btnGetData.setOnClickListener(this);
+        tvCourseData = (TextView) findViewById(R.id.course_data);
+        etChildPath = (EditText) findViewById(R.id.child_path);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == btnGetData){
+            String input = etChildPath.getText().toString().replace(" ", "").toUpperCase();
+            Log.d("TEST", "child path: " + input);
+            String dept = input.replaceAll("\\d", "");
+            String composedPath = dept + "/" + input;
+            if(!input.isEmpty()){
+                firebaseDB.child(composedPath).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        courseData data = dataSnapshot.getValue(courseData.class);
+                        if(data != null){
+                            tvCourseData.setText(data.toString());
+                        }else{
+                            Toast.makeText(MainActivity.this, "Please make sure you input correct course number", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) { }
+                });
+            }else{
+                Toast.makeText(this, "Please specify query path", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
