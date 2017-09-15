2d87c345c57fd064c7057d57b6bc6d5df0923f3f
Junjie Chen <wiiso853@gmail.com>
2017-09-14 23:55:20 -0400

    added: login logic
    Now user can login with UB email or proceed as a guest

--- app/src/main/java/cse442/courseradar/MainActivity.java
+++ app/src/main/java/cse442/courseradar/MainActivity.java
@@ -1,63 +1,30 @@
 package cse442.courseradar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import static java.security.AccessController.getContext;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
-    private DatabaseReference firebaseDB;
-    private Button btnGetData;
-    private TextView tvCourseData;
-    private EditText etChildPath;
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        firebaseDB = FirebaseDatabase.getInstance().getReference();
-        btnGetData = (Button) findViewById(R.id.get_data);
-        btnGetData.setOnClickListener(this);
-        tvCourseData = (TextView) findViewById(R.id.course_data);
-        etChildPath = (EditText) findViewById(R.id.child_path);
     }
 
-    @Override
-    public void onClick(View view) {
-        if(view == btnGetData){
-            String input = etChildPath.getText().toString().replace(" ", "").toUpperCase();
-            Log.d("TEST", "child path: " + input);
-            String dept = input.replaceAll("\\d", "");
-            String composedPath = dept + "/" + input;
-            if(!input.isEmpty()){
-                firebaseDB.child(composedPath).addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        courseData data = dataSnapshot.getValue(courseData.class);
-                        if(data != null){
-                            tvCourseData.setText(data.toString());
-                        }else{
-                            Toast.makeText(MainActivity.this, "Please make sure you input correct course number", Toast.LENGTH_SHORT).show();
-                        }
-                    }
 
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) { }
-                });
-            }else{
-                Toast.makeText(this, "Please specify query path", Toast.LENGTH_SHORT).show();
-            }
-        }
+    /* override the back button logic, press back will go to home screen
+     * this is a workaround to avoid signing user out if the app is cleaned from memory or this activity is destroyed */
+    @Override
+    public void onBackPressed() {
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
     }
 }
